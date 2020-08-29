122574ceb586a61325e834e1a41f01c3c86195f9
2gracz <andrzej.lenarczyk.5@gmail.com>
2020-08-29 14:23:17 +0200

    Use AOSP Surfaceflinger

--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -2,7 +2,7 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
-
+TARGET_USES_AOSP_SURFACEFLINGER := true
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
