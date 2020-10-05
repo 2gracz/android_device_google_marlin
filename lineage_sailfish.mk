6b3e8a37d319ded8f591e124c06ef7fe755e11d5
2gracz <andrzej.lenarczyk.5@gmail.com>
2020-10-05 19:49:23 +0200

    bootleggify

--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/bootleg/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 
--include device/google/marlin/sailfish/device-lineage.mk
+-include device/google/marlin/sailfish/device-bootleg.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := bootleg_sailfish
